8dfb503be5f66b6785f6e01c53ca4accc678269e
Edward Anchundia <edward.anchundia@gmail.com>
2017-03-19 02:22:30 -0400

    stuff

--- Navty/Navty/AppDelegate.swift
+++ Navty/Navty/AppDelegate.swift
@@ -46,15 +46,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         
-        let userDefaults = UserDefaults.standard
+        //let userDefaults = UserDefaults.standard
         
-        if userDefaults.object(forKey: "ApplicationIdentifier") == nil {
-            let UUID = NSUUID().uuidString
-            userDefaults.set(UUID, forKey: "ApplicationIdentifier")
-            userDefaults.synchronize()
-        }
-        print("HEREEEE")
-        print(UserDefaults.standard.value(forKey: "ApplicationIdentifier")!)
+//        if userDefaults.object(forKey: "ApplicationIdentifier") == nil {
+//            let UUID = NSUUID().uuidString
+//            userDefaults.set(UUID, forKey: "ApplicationIdentifier")
+//            userDefaults.synchronize()
+//        }
+//        print("HEREEEE")
+//        print(UserDefaults.standard.value(forKey: "ApplicationIdentifier")!)
         
         
         let actionOne = UNNotificationAction(identifier: "agree", title: "Ok", options: [.foreground])
@@ -168,6 +168,7 @@ extension AppDelegate: CLLocationManagerDelegate {
             handleEvent(forRegion: region)
         }
     }
+    
     
 }
 
